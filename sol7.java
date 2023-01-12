a614ced21162b3d51ea6374e0b09d2da912a33b0
Michael Mandich <85703638+michaelmandich@users.noreply.github.com>
2023-01-11 22:55:35 -0500

    Update sol7.java

--- sol7.java
+++ sol7.java
@@ -3,15 +3,15 @@ class Solution {
         double holding = 0;
         int temp = x;  
         while ((temp/10) != 0){
-            holding = holding*10 + (temp%10);
+            holding = holding*10 + (temp%10); \\ this loop is to get all but the last digit, so it stop directly before that when the number is down to it's last digit
             temp = temp/10;
         }
         holding = holding*10 + (temp%10);
-        temp = temp/10;
+        temp = temp/10; \\ two more lines to get the last digit after while loop stops
         if (holding<=-2147483647||holding>=2147483647){
-            return 0; 
+            return 0; \\ error handling
         }
-        int backup = (int)holding;
+        int backup = (int)holding; \\casting the double back into an integer in the case that it was not too large. 
         return backup; 
     }
 }
