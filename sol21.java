58ef91051e2234c757aa4894670603fa06ee8b01
Michael Mandich <85703638+michaelmandich@users.noreply.github.com>
2023-01-12 20:07:16 -0500

    Update sol21.java

--- sol21.java
+++ sol21.java
@@ -4,7 +4,7 @@ class Solution {
         for(int index=1;index<strs.length;index++){
             while(strs[index].indexOf(prefix) != 0){
                 prefix=prefix.substring(0,prefix.length()-1); \\ the core of this algorithm, basically looks at each string and if it doesn't line up with any of the
-                \\ other prefixes
+                \\ other prefixes or similar letters in each, it moves on until it finds one that it does, and it does this for every string in the string list. 
             }
         }
         return prefix;
