98634f39029e96d71b1bacb2e0768ce55b294111
Michael Mandich <85703638+michaelmandich@users.noreply.github.com>
2023-01-12 20:06:26 -0500

    Create sol21.java

---
+++ sol21.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        String prefix = strs[0];
+        for(int index=1;index<strs.length;index++){
+            while(strs[index].indexOf(prefix) != 0){
+                prefix=prefix.substring(0,prefix.length()-1); \\ the core of this algorithm, basically looks at each string and if it doesn't line up with any of the
+                \\ other prefixes
+            }
+        }
+        return prefix;
+    }
+}
+\\ a program that will take the longest "common prefix" meaning the longest string of letters in a string in the string array strs. 
